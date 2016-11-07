35d09124d5e44279f2398959e43e81beebbe785e
hcastro1989 <castroh@us.ibm.com>
2016-11-07 11:09:05 -0500

    updated with chart data script and attempts to makechart

--- OI Sector Script.R
+++ OI Sector Script.R
@@ -3,6 +3,7 @@
 library(dplyr)
 library(tidyr)
 library(lubridate)
+library(ggplot2)
 
 #OI by Sector
 oi.sector.1H2015 <- cube.F %>%
@@ -24,3 +25,13 @@ oi.na.2015 <- cube.F %>%
 oi.na.2015 <- cube.F %>% 
   filter(create.m.yr >= ymd("2016-01-01") & create.m.yr <= ymd("2016-06-30")) %>%
   summarise(na.total.oi = sum(Total.TCV))
+
+#OI NA 2014 - 2016
+oi.2014.2016.chart <- cube.F %>%
+  filter(create.m.yr >= ymd("2014-01-01") & create.m.yr <= ymd("2016-09-30")) %>% 
+  group_by(create.m.yr) %>%
+  summarise(na.monthly.tcv = sum(Total.TCV))
+
+#OI NA Chart
+ggplot(oi.2014.2016.chart,aes(x = create.m.yr, y = (na.monthly.tcv/1000000000)))
+ 
