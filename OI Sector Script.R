b5db58fd873c2fd19e313783e5c603089787b7e8
hcastro1989 <castroh@us.ibm.com>
2016-11-07 13:41:48 -0500

    Updated for new charts by quarter

--- OI Sector Script.R
+++ OI Sector Script.R
@@ -5,25 +5,40 @@ library(tidyr)
 library(lubridate)
 library(ggplot2)
 
-#OI by Sector
-oi.sector.1H2015 <- cube.F %>%
-  filter(create.m.yr >= ymd("2015-01-01") & create.m.yr <= ymd("2015-06-30")) %>%
-  group_by(Sector) %>%
-  summarise(sector.total.oi = sum(Total.TCV))
+load("~/SametimeFileTransfers/cube.saved")
 
-oi.sector.1H2016 <- cube.F %>%
-  filter(create.m.yr >= ymd("2016-01-01") & create.m.yr <= ymd("2016-06-30")) %>%
+#OI by Sector by quarter
+oi.sector.1Q2016 <- cube.F %>%
+  filter(create.m.yr >= ymd("2016-01-01") & create.m.yr <= ymd("2016-03-30")) %>%
   group_by(Sector) %>%
-  summarise(sector.total.oi = sum(Total.TCV))
+  summarise(oi.sector.quarter = sum(Total.TCV))
 
-#OI NA 2015
-oi.na.2015 <- cube.F %>% 
-  filter(create.m.yr >= ymd("2015-01-01") & create.m.yr <= ymd("2015-06-30")) %>%
+oi.sector.2Q2016 <- cube.F %>%
+  filter(create.m.yr >= ymd("2016-04-01") & create.m.yr <= ymd("2016-06-30")) %>%
+  group_by(Sector) %>%
+  summarise(oi.sector.quarter = sum(Total.TCV))
+
+oi.sector.3Q2016 <- cube.F %>%
+  filter(create.m.yr >= ymd("2016-07-01") & create.m.yr <= ymd("2016-09-30")) %>%
+  group_by(Sector) %>%
+  summarise(oi.sector.quarter = sum(Total.TCV))
+
+
+#OI NA total 2016 & Quarter
+oi.na.2016 <- cube.F %>% 
+  filter(create.m.yr >= ymd("2016-01-01") & create.m.yr <= ymd("2016-09-30")) %>%
   summarise(na.total.oi = sum(Total.TCV))
 
-#OI NA 2016
-oi.na.2015 <- cube.F %>% 
-  filter(create.m.yr >= ymd("2016-01-01") & create.m.yr <= ymd("2016-06-30")) %>%
+oi.na.1Q2016 <- cube.F %>% 
+  filter(create.m.yr >= ymd("2016-01-01") & create.m.yr <= ymd("2016-03-30")) %>%
+  summarise(na.total.oi = sum(Total.TCV))
+
+oi.na.2Q2016 <- cube.F %>% 
+  filter(create.m.yr >= ymd("2016-04-01") & create.m.yr <= ymd("2016-06-30")) %>%
+  summarise(na.total.oi = sum(Total.TCV))
+
+oi.na.3Q2016 <- cube.F %>% 
+  filter(create.m.yr >= ymd("2016-07-01") & create.m.yr <= ymd("2016-09-30")) %>%
   summarise(na.total.oi = sum(Total.TCV))
 
 #OI NA 2014 - 2016
