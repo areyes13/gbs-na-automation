1fa8a90db392502bb7cac7b34dbe5a72d6a1f7f6
hcastro1989 <castroh@us.ibm.com>
2016-11-11 13:09:40 -0500

    removing space junk from code

--- Yield Sector Script.R
+++ Yield Sector Script.R
@@ -8,22 +8,22 @@ load("~/SametimeFileTransfers/cubes.saved")
 yield.sector.1Q2016 <- closed.cube.F %>%
   filter(date >= ymd("2016-01-01") & date <=ymd("2016-03-30")) %>%
   group_by(Sector) %>%
-  summarise(yield = sum(Won)/sum(Won, Not.Won, Space.Junk))
+  summarise(yield = sum(Won)/sum(Won, Not.Won))
 
 yield.sector.2Q2016 <- closed.cube.F %>%
   filter(date >= ymd("2016-04-01") & date <=ymd("2016-06-30")) %>%
   group_by(Sector) %>%
-  summarise(yield = sum(Won)/sum(Won, Not.Won, Space.Junk))
+  summarise(yield = sum(Won)/sum(Won, Not.Won))
 
 yield.sector.3Q2016 <- closed.cube.F %>%
   filter(date >= ymd("2016-07-01") & date <=ymd("2016-09-30")) %>%
   group_by(Sector) %>%
-  summarise(yield = sum(Won)/sum(Won, Not.Won, Space.Junk))
+  summarise(yield = sum(Won)/sum(Won, Not.Won))
 
 yield.sector.2016 <- closed.cube.F %>%
   filter(date >= ymd("2016-01-01") & date <=ymd("2016-09-30")) %>%
   group_by(Sector) %>%
-  summarise(yield = sum(Won)/sum(Won, Not.Won, Space.Junk))
+  summarise(yield = sum(Won)/sum(Won, Not.Won))
 
 write.csv(yield.sector.1Q2016, "yield.sector.1Q2016.csv")
 write.csv(yield.sector.2Q2016, "yield.sector.2Q2016.csv")
@@ -35,19 +35,19 @@ write.csv(yield.sector.2016, "yield.sector.2016.csv")
 
 yield.na.1Q2016 <- closed.cube.F %>%
   filter(date >= ymd("2016-01-01") & date<=ymd("2016-03-30")) %>%
-  summarise(yield = sum(Won)/ sum(Won, Not.Won, Space.Junk))
+  summarise(yield = sum(Won)/ sum(Won, Not.Won))
 
 yield.na.2Q2016 <- closed.cube.F %>%
   filter(date >= ymd("2016-04-01") & date<=ymd("2016-06-30")) %>%
-  summarise(yield = sum(Won)/ sum(Won, Not.Won, Space.Junk))
+  summarise(yield = sum(Won)/ sum(Won, Not.Won))
 
 yield.na.3Q2016 <- closed.cube.F %>%
   filter(date >= ymd("2016-07-01") & date<=ymd("2016-09-30")) %>%
-  summarise(yield = sum(Won)/ sum(Won, Not.Won, Space.Junk))
+  summarise(yield = sum(Won)/ sum(Won, Not.Won))
 
 yield.na.2016 <- closed.cube.F %>%
   filter(date >= ymd("2016-01-01") & date<=ymd("2016-09-30")) %>%
-  summarise(yield = sum(Won)/ sum(Won, Not.Won, Space.Junk))
+  summarise(yield = sum(Won)/ sum(Won, Not.Won))
 
 write.csv(yield.na.1Q2016, "yield.na.1Q2016.csv")
 write.csv(yield.na.2Q2016, "yield.na.2Q2016.csv")
@@ -59,6 +59,6 @@ write.csv(yield.na.2016, "yield.na.2016.csv")
 yield.2014.2016.chart <- closed.cube.F %>%
   filter(date >= ymd("2014-01-01") & date <= ymd("2016-09-30")) %>% 
   group_by(date) %>%
-  summarise(yield = sum(Won)/ sum(Won, Not.Won, Space.Junk))
+  summarise(yield = sum(Won)/ sum(Won, Not.Won))
 
 write.csv(yield.2014.2016.chart, "yield.2014.2016.chart.csv")
